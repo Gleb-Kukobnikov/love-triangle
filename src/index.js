6d0c41a40654e3e83b4d8d7d6ed9a08d5f72bec7
Gleb <31860789+Gleb-Kukobnikov@users.noreply.github.com>
2018-02-26 20:08:29 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+    for (i = 0; i < preferences.length; i++) {
+        let a = preferences[i];
+        let b = preferences[i + 1];
+        let c = preferences[i + 2]
+
+        if (a + 1 == b && b == c + 2) {
+            count++;
+        }
+    }
+    return count;
 };
